6fae35fc23ab4e8b89ae4fa636dd45e92e4d4565
DaggupatiPavan <97659712+DaggupatiPavan@users.noreply.github.com>
2023-01-27 20:13:41 +0530

    Update modify.sh

--- modify.sh
+++ modify.sh
@@ -6,7 +6,7 @@ echo "listen=udp:$(cat PRI_IP):5060 advertise $(cat PUB_IP):5060" >> kam.cfg
 echo "listen=tcp:$(cat PRI_IP):5060 advertise $(cat PUB_IP):5060" >> kam.cfg
 echo "alias=localhost:5060" >> kam.cfg
 echo "auto_aliases=no" >> kam.cfg 
-echo "advertised_address=$(cat PUB_IP) >> kam.cfg 
+echo "advertised_address=$(cat PUB_IP)" >> kam.cfg 
 
 
 sudo docker cp kam.cfg kamailio:/etc/kamailio/kamailio.cfg 
