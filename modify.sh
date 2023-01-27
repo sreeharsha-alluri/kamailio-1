304d1cd20d4ea9268a3893e8e2a9a786d765a4c4
DaggupatiPavan <97659712+DaggupatiPavan@users.noreply.github.com>
2023-01-27 20:56:04 +0530

    Update modify.sh

--- modify.sh
+++ modify.sh
@@ -2,15 +2,15 @@
 
 hostname -I | cut -d " " -f 1 > PRI_IP
 curl ifconfig.me > PUB_IP
-echo "listen=udp:$(cat PRI_IP):5060 advertise $(cat PUB_IP):5060" >> kam.cfg
-echo "listen=tcp:$(cat PRI_IP):5060 advertise $(cat PUB_IP):5060" >> kam.cfg
-echo "alias=localhost:5060" >> kam.cfg
-echo "auto_aliases=no" >> kam.cfg 
-echo "advertised_address=$(cat PUB_IP)" >> kam.cfg 
+echo "listen=udp:$(cat PRI_IP):5060 advertise $(cat PUB_IP):5060" >> kamailio/kam.cfg
+echo "listen=tcp:$(cat PRI_IP):5060 advertise $(cat PUB_IP):5060" >> kamailio/kam.cfg
+echo "alias=localhost:5060" >> kamailio/kam.cfg
+echo "auto_aliases=no" >> kamailio/kam.cfg 
+echo "advertised_address=$(cat PUB_IP)" >> kamailio/kam.cfg 
 
 
-sudo docker cp kam.cfg kamailio:/etc/kamailio/kamailio.cfg 
+sudo docker cp kamailio/kam.cfg kamailio:/etc/kamailio/kamailio.cfg 
 
-sudo docker cp kamailio kamailio:/etc/default/kamailio 
+sudo docker cp kamailio/kamailio kamailio:/etc/default/kamailio 
 
 
