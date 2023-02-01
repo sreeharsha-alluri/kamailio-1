62c5171563dc487edaffc4aa748b824d27f01a44
DaggupatiPavan <97659712+DaggupatiPavan@users.noreply.github.com>
2023-02-01 22:04:49 +0530

    Update NetDevOps.py

--- NetDevOps.py
+++ NetDevOps.py
@@ -2,21 +2,37 @@ import os
 import argparse
 import requests
 
+# Get the public IP address
+response = requests.get('https://ipv4.icanhazip.com')
+PUBLIC_IP = response.text.strip()
+
+# Parse input parameters
 parser = argparse.ArgumentParser()
-parser.add_argument("--validate", required=True)
-parser.add_argument("--service", required=True)
-parser.add_argument("--cloud", required=True)
-parser.add_argument("--form_factor", required=True)
+parser.add_argument('--validate', required=True)
+parser.add_argument('--service', required=True)
+parser.add_argument('--cloud', required=True)
+parser.add_argument('--form_factor', required=True)
 args = parser.parse_args()
 
-# Get public IP
-response = requests.get("https://ipv4.icanhazip.com")
-public_ip = response.text.strip()
+# Validate input parameters
+if args.validate not in ['full', 'small', 'medium']:
+    print("Invalid value for parameter validate: {}".format(args.validate))
+    print("Accepted values are: full, small, or medium")
+    exit(1)
 
-# Check if cloud parameter is valid
-if args.cloud not in ["Aws", "Azure"]:
-    print(f"Invalid value for parameter cloud: {args.cloud}")
+if args.cloud not in ['Aws', 'Azure']:
+    print("Invalid value for parameter cloud: {}".format(args.cloud))
     print("Accepted values are: Aws or Azure")
+    exit(1)
+
+if args.service != 'IMS':
+    print("Invalid value for parameter service: {}".format(args.service))
+    print("Accepted values are: IMS")
+    exit(1)
+
+if args.form_factor != 'CNF':
+    print("Invalid value for parameter form_factor: {}".format(args.form_factor))
+    print("Accepted values are: CNF")
     exit(1)
 
 # Execute the Jenkins CLI commands with the provided IP and parameters
